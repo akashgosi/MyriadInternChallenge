dbc838c18bea392ee55a21056e76cf49a3c8fbad
akashgosi <gosi.akash@gmail.com>
2015-03-14 05:23:38 -0500

    Setup code to get the kingdom from the API

---
+++ app/src/main/java/com/akash/gosi/myriadinternchallenge/Kingdoms.java
@@ -0,0 +1,34 @@
+package com.akash.gosi.myriadinternchallenge;
+
+/**
+ * Created by Akash on 3/14/2015.
+ */
+public class Kingdoms {
+    private String id;
+    private String Name;
+    private String imgUrl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
